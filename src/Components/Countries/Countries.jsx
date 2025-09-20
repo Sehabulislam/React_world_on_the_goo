1d9cf16879b8f47b8ce35dae595d25cd2c4301b4
Sehabulislam <shehabulislam244@gmail.com>
2025-09-20 12:11:37 +0600

    added the countries style

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,16 +1,19 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries ;
 
     return (
-        <div>
+        <div >
             <h1>In the Countries : {countries.length}</h1>
-            {
+            <div className='countries'>
+                {
                 countries.map(country => <Country key={country.ccn3.ccn3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
     console.log(country);
+    
+    const handleVisited =()=>{
+        console.log("Visited Country");
+    }
+    
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2> Name : {country.name.common}</h2>
             <h4>Capital : {country.capital.capital}</h4>
-            <h4> Population : {country.population.population}</h4>
+            <h4> Population : {country.population.population} {country.population.population > 500000 ? "Big Country" : "Small Country"}</h4>
             <p>Region : {country.region.region}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
